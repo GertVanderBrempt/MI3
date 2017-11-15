2fff7970394837d2f4f608c0f6d785b58f5597cf
Ruben Godeau <rubengodeau@users.noreply.github.com>
2017-11-15 14:33:58 +0100

    Update getfitdb.php

--- getfitdb.php
+++ getfitdb.php
@@ -1,13 +1,13 @@
 <?php
-    $servername = "fdb13.awardspace.net"; // de servernaam die je van je hosting firma hebt ontvangen
-    $username   = "root"; // de gebruikersnaam die je van je hosting firma hebt ontvangen
-    $password   = "fwyr6wk6DBwuHG7P"; // het paswoord dat je van je hosting firma hebt ontvangen
+    $servername = "fdb13.awardspace.net:3306"; // de servernaam die je van je hosting firma hebt ontvangen
+    $username   = "2518084_getfit"; // de gebruikersnaam die je van je hosting firma hebt ontvangen
+    $password   = "getfit2017"; // het paswoord dat je van je hosting firma hebt ontvangen
     $dbname     = "2518084_getfit"; // de naam van de databank die je van je hosting firma hebt ontvangen
     
     // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die(mysqli_connect_error());
 
-        $result = $conn -> query($id == null ? "SELECT * FROM tblGebruiker");
+        $result = $conn -> query("SELECT * FROM tblGebruiker");
 
         // maak van de inhoud van deze result een json object waarvan
         // ook in android de juiste gegeventypes herkend worden
