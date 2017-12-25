67686b5834c5c75cdda9fd3663fd3c4bf2bed904
Ruben Godeau <rubengodeau@users.noreply.github.com>
2017-12-25 15:47:10 +0100

    routine start

--- Cordova/project-getfit/dist/js/my-app.js
+++ Cordova/project-getfit/dist/js/my-app.js
@@ -2,7 +2,11 @@
 var myApp = new Framework7();
 // Export selectors engine
 var $$ = Dom7;
+var GEB_ID = null;
 var GEB_Voornaam = null;
+var GEB_Familienaam = null;
+var rouForGeb = [];
+var rouForNotGeb = [];
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
@@ -52,11 +56,13 @@ function RegistreerGebruiker() {
             }
         });
     }
-}myApp.onPageInit('login', function (page) {
+}
+myApp.onPageInit('login', function (page) {
     $$('.gf-btn-login-gebruiker').on('click', function () {
         LoginGebruiker();
     });
 });
+
 function LoginGebruiker() {
     //event.preventDefault();
     //console.log('click login');
@@ -65,30 +71,60 @@ function LoginGebruiker() {
     data.GEB_Email = $(".gf-input-email").val();
     data.GEB_Wachtwoord = $(".gf-input-wachtwoord").val();
     //else {
-        data.GEB_Wachtwoord = CryptoJS.MD5(data.GEB_Wachtwoord).toString();
-        $$.ajax({
-            type: "POST"
-            , url: 'http://getfit.getenjoyment.net/getfitdb.php'
-            , crossDomain: true
-            , data: data
-            , withCredentials: false
-            , success: function (responseData, textStatus, jqXHR) {
-                if((JSON.parse(responseData)).status === 200){
-                    GEB_Voornaam = (JSON.parse(responseData)).data[0]["GEB_Voornaam"];
-                    $$(".gf-txt-warning").html(GEB_Voornaam +" werd met success ingelogd");
-                    $$(".gf-txt-warning").css("color", "green");
-                     mainView.router.loadPage({url:'home.html', ignoreCache:true, reload:true });
-                }
-                else{
-                    $$(".gf-txt-warning").html((JSON.parse(responseData)).data);
-                    $$(".gf-txt-warning").css("color", "red");
+    data.GEB_Wachtwoord = CryptoJS.MD5(data.GEB_Wachtwoord).toString();
+    $$.ajax({
+        type: "POST"
+        , url: 'http://getfit.getenjoyment.net/getfitdb.php'
+        , crossDomain: true
+        , data: data
+        , withCredentials: false
+        , success: function (responseData, textStatus, jqXHR) {
+            if ((JSON.parse(responseData)).status === 200) {
+                GEB_ID = (JSON.parse(responseData)).data[0]["GEB_ID"];
+                if (GEB_ID != null) {
+                    var dataGeb = {};
+                    dataGeb.bewerking = "getGeb";
+                    dataGeb.GEB_ID = GEB_ID;
+                    $$.ajax({
+                        type: "POST"
+                        , url: 'http://getfit.getenjoyment.net/getfitdb.php'
+                        , crossDomain: true
+                        , data: dataGeb
+                        , withCredentials: false
+                        , success: function (responseData, textStatus, jqXHR) {
+                            if ((JSON.parse(responseData)).status === 200) {
+                                GEB_Voornaam = (JSON.parse(responseData)).data[0]["GEB_Voornaam"];
+                                GEB_Familienaam = (JSON.parse(responseData)).data[0]["GEB_Familienaam"];
+                                $$(".gf-txt-warning").html(GEB_Voornaam + " werd met success ingelogd");
+                                $$(".gf-txt-warning").css("color", "green");
+                                mainView.router.loadPage({
+                                    url: 'home.html'
+                                    , ignoreCache: true
+                                    , reload: true
+                                });
+                            }
+                            else {
+                                $$(".gf-txt-warning").html((JSON.parse(responseData)).data);
+                                $$(".gf-txt-warning").css("color", "red");
+                            }
+                        }
+                        , error: function (responseData, textStatus, errorThrown) {
+                            $$(".gf-txt-warning").html("Er ging iets mis tijdens het versturen :" + errorThrown);
+                            $$(".gf-txt-warning").css("color", "red");
+                        }
+                    });
                 }
             }
-            , error: function (responseData, textStatus, errorThrown) {
-                $$(".gf-txt-warning").html("Er ging iets mis tijdens het versturen :" + errorThrown);
+            else {
+                $$(".gf-txt-warning").html((JSON.parse(responseData)).data);
                 $$(".gf-txt-warning").css("color", "red");
             }
-        });
+        }
+        , error: function (responseData, textStatus, errorThrown) {
+            $$(".gf-txt-warning").html("Er ging iets mis tijdens het versturen :" + errorThrown);
+            $$(".gf-txt-warning").css("color", "red");
+        }
+    });
     //}
 }
 //
@@ -96,10 +132,124 @@ myApp.onPageInit('home', function (page) {
     /*$$('.gf-btn-login-gebruiker').on('click', function () {
         LoginGebruiker();
     });*/
-    if(GEB_Voornaam != null){
-        $$(".content-block-title").html("Welkom " + GEB_Voornaam);
+    if (GEB_Voornaam != null) {
+        $$(".content-block-title").html("Welkom " + GEB_Voornaam + " " + GEB_Familienaam);
     }
 });
+myApp.onPageInit('routines', function (page) {//traag
+    var dataGeb = {};
+    dataGeb.bewerking = "getRouForGeb";
+    dataGeb.GEB_ID = GEB_ID;
+    $$.ajax({
+        type: "POST"
+        , url: 'http://getfit.getenjoyment.net/getfitdb.php'
+        , crossDomain: true
+        , data: dataGeb
+        , withCredentials: false
+        , success: function (responseData, textStatus, jqXHR) {
+            if ((JSON.parse(responseData)).status === 200) {
+                var html = "";
+                (JSON.parse(responseData)).data.forEach(function (dataRou) {
+                    console.log(dataRou);
+                    var ROU_ID = dataRou["ROU_ID"];
+                    var ROU_Naam = dataRou["ROU_Naam"];
+                    html += "<p data-value-id='" + ROU_ID + "' >";
+                    html += ROU_Naam;
+                    html += "</p>";
+                });
+                rouForGeb = html;
+                //$$(".mijn-routines").html(html);
+                if(html.length > 0){
+                $$(".mijn-routines").html(html);
+                }else{
+                $$(".mijn-routines").html("<p style='color:red;' >"+"geen"+"</p>");
+                }
+            }
+            else {
+                //$$(".gf-txt-warning").html((JSON.parse(responseData)).data);
+                //$$(".gf-txt-warning").css("color", "red");
+            }
+        }
+        , error: function (responseData, textStatus, errorThrown) {
+            //$$(".gf-txt-warning").html("Er ging iets mis tijdens het versturen :" + errorThrown);
+            //$$(".gf-txt-warning").css("color", "red");
+        }
+    });
+    var dataGeb2 = {};
+    dataGeb2.bewerking = "getRouForNotGeb";
+    dataGeb2.GEB_ID = GEB_ID;
+    $$.ajax({
+        type: "POST"
+        , url: 'http://getfit.getenjoyment.net/getfitdb.php'
+        , crossDomain: true
+        , data: dataGeb2
+        , withCredentials: false
+        , success: function (responseData, textStatus, jqXHR) {
+            if ((JSON.parse(responseData)).status === 200) {
+                var html = "";
+                (JSON.parse(responseData)).data.forEach(function (dataRou) {
+                    console.log(dataRou);
+                    var ROU_ID = dataRou["ROU_ID"];
+                    var ROU_Naam = dataRou["ROU_Naam"];
+                    html += "<p data-value-id='" + ROU_ID + "'>";
+                    html += ROU_Naam;
+                    html += "</p>";
+                });
+                rouForNotGeb = html;
+                //console.log(html);
+                if(html.length > 0){
+                $$(".andere-routines").html(html);
+                }else{
+                $$(".andere-routines").html("geen");
+                }
+            }
+            else {
+                //$$(".gf-txt-warning").html((JSON.parse(responseData)).data);
+                //$$(".gf-txt-warning").css("color", "red");
+            }
+        }
+        , error: function (responseData, textStatus, errorThrown) {
+            //$$(".gf-txt-warning").html("Er ging iets mis tijdens het versturen :" + errorThrown);
+            //$$(".gf-txt-warning").css("color", "red");
+        }
+    });
+});
+myApp.onPageInit('maakroutine', function (page) {
+    $$('.gf-btn-addRou').on('click', function () {
+        AddRou();
+    });
+});
+
+function AddRou() {
+    //event.preventDefault();
+    //console.log('click login');
+    var data = {};
+    data.bewerking = "addRou";
+    data.ROU_Naam = $(".gf-input-naam").val();
+    data.ROU_GEB_ID = GEB_ID;
+    $$.ajax({
+        type: "POST"
+        , url: 'http://getfit.getenjoyment.net/getfitdb.php'
+        , crossDomain: true
+        , data: data
+        , withCredentials: false
+        , success: function (responseData, textStatus, jqXHR) {
+            if ((JSON.parse(responseData)).status === 200) {
+                $$(".gf-txt-warning").html((JSON.parse(responseData)).data[0]);
+                $$(".gf-txt-warning").css("color", "green");
+            }
+            else {
+                $$(".gf-txt-warning").html((JSON.parse(responseData)).data);
+                $$(".gf-txt-warning").css("color", "red");
+            }
+        }
+        , error: function (responseData, textStatus, errorThrown) {
+            $$(".gf-txt-warning").html("Er ging iets mis tijdens het versturen :" + errorThrown);
+            $$(".gf-txt-warning").css("color", "red");
+        }
+    });
+    //}
+}
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('page1', function (page) {
     // run createContentPage func after link was clicked
